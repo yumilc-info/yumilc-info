db34f6535f69fff4df66d3ef2e405fb527721726
iorin-io <iodere77@gmail.com>
2024-03-04 18:02:40 +0900

    font定義

---
+++ src/const/font.ts
@@ -0,0 +1,22 @@
+import { Montserrat } from "next/font/google";
+import { Zen_Maru_Gothic } from "next/font/google";
+
+export const Montserrat400 = Montserrat({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export const Montserrat900 = Montserrat({
+	weight: "900",
+	subsets: ["latin"],
+});
+
+export const Zen_Maru_Gothic400 = Zen_Maru_Gothic({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export const Zen_Maru_Gothic500 = Zen_Maru_Gothic({
+	weight: "500",
+	subsets: ["latin"],
+});
