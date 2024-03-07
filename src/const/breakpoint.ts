b20980c6bc394ce6bb38ddf6ae475cba2697065d
iorin-io <iodere77@gmail.com>
2024-03-07 14:47:50 +0900

    breakpointsの定義

---
+++ src/const/breakpoint.ts
@@ -0,0 +1,12 @@
+// useMediaQueries.js
+import { useMediaQuery } from "react-responsive";
+
+export const useLgQuery = (): boolean => useMediaQuery({ minWidth: 1024 });
+
+export const LgQuery = (): boolean =>
+	useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+
+export const useMdQuery = (): boolean =>
+	useMediaQuery({ minWidth: 640, maxWidth: 767 });
+
+export const useSmQuery = (): boolean => useMediaQuery({ maxWidth: 639 });
