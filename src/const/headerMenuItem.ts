8dd83b27b3032e7ad8d52dce699e2c2813a52241
iorin-io <iodere77@gmail.com>
2024-04-04 19:50:35 +0900

    headerMenuItemを変更

--- src/const/headerMenuItem.ts
+++ src/const/headerMenuItem.ts
@@ -1,6 +1,5 @@
 export const menuItems = [
 	{ label: "Home", href: "/" },
 	{ label: "About", href: "/about" },
-	{ label: "Services", href: "/services" },
-	{ label: "Contact", href: "/contact" },
+	{ label: "News", href: "/news" },
 ];
