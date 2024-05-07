6cc5b79805c5cf7a508ab6679fd6519e21cb1067
iorin-io <iodere77@gmail.com>
2024-05-07 13:33:28 +0900

    わからん

--- src/app/news/ex/page.tsx
+++ src/app/news/ex/page.tsx
@@ -9,8 +9,11 @@ import { SnsImageLink } from "@/components/ContactsImageLink";
 import { HoverGrowWrapper } from "@/components/HoverGrowWrapper";
 
 // consts
-import { Montserrat900 } from "@/const/font";
+import { Montserrat400, Montserrat900, ZenMaruGothic400 } from "@/const/font";
 import { snsX, snsInstagram, snsYoutube } from "@/const/TopPageText";
+import Link from "next/link";
+import Image from "next/image";
+import { formatDate } from "@/libs/formatDate";
 
 // images
 
@@ -41,8 +44,153 @@ const snsFlex = css({
 	flexDirection: "row",
 	margin: "20px 0",
 });
+const titleStyle = css({
+	fontSize: {
+		base: "24px",
+		md: "32px",
+	},
+	color: "#4C4C4C",
+	paddingTop: "20px",
+	paddingBottom: "10px",
+});
+const dateStyle = css({
+	color: "#4C4C4C",
+	fontSize: {
+		base: "16px",
+		md: "20px",
+	},
+});
+const snsImageStyle = css({
+	position: "relative",
+	width: {
+		base: "20px",
+		md: "24px",
+	},
+	height: {
+		base: "20px",
+		md: "24px",
+	},
+	marginLeft: "10px",
+});
+
+const articleStyle = css({
+	color: "#4C4C4C",
+	letterSpacing: "0.1em",
+	lineHeight: "2em",
+	fontSize: {
+		base: "14px",
+		md: "16px",
+	},
+	"& p": {
+		marginLeft: "10px",
+		"& code": {
+			borderRadius: "5px",
+		},
+	},
+	"& h1": {
+		fontSize: "22px",
+		fontWeight: "bold",
+		marginTop: "20px",
+		marginBottom: "10px",
+		borderBottom: "1px solid #ddd",
+	},
+	"& h2": {
+		fontSize: "20px",
+		fontWeight: "bold",
+		marginTop: "20px",
+		marginBottom: "10px",
+		borderBottom: "1px solid #ddd",
+	},
+	"& h3": {
+		fontSize: "18px",
+		fontWeight: "bold",
+		marginTop: "20px",
+		marginBottom: "10px",
+		borderBottom: "1px solid #ddd",
+	},
+	"& h4": {
+		fontSize: "17px",
+		fontWeight: "bold",
+		marginTop: "20px",
+		marginBottom: "10px",
+		borderBottom: "1px solid #ddd",
+	},
+	"& h5": {
+		fontSize: "16px",
+		fontWeight: "bold",
+		marginTop: "20px",
+		marginBottom: "10px",
+		borderBottom: "1px solid #ddd",
+	},
+	"& blockquote": {
+		borderLeft: "5px solid #ddd",
+		paddingLeft: "10px",
+		margin: "10px",
+	},
+	"& pre": {
+		"& code": {
+			margin: "10px",
+			padding: "25px !important",
+			fontSize: {
+				base: "12px",
+				md: "14px",
+			},
+			letterSpacing: "0",
+			lineHeight: "normal",
+			borderRadius: "10px",
+		},
+	},
+	"& table": {
+		borderCollapse: "collapse",
+		margin: "10px",
+	},
+	"& th": {
+		border: "1px solid #ddd",
+		padding: "10px",
+		"& p": {
+			marginLeft: "0",
+		},
+		backgroundColor: "#f2f2f2",
+	},
+	"& td": {
+		border: "1px solid #ddd",
+		padding: "10px",
+		"& p": {
+			marginLeft: "0",
+		},
+	},
+	"& ul": {
+		marginLeft: "20px",
+		marginTop: "10px",
+		marginBottom: "10px",
+		listStyleType: "circle",
+	},
+	"& ol": {
+		marginLeft: "20px",
+		marginTop: "10px",
+		marginBottom: "10px",
+		listStyleType: "decimal",
+	},
+});
 
 export default function Home(): JSX.Element {
+	const postId = "1";
+	const post = {
+		title: "title",
+		publishedAt: "2021-01-01",
+		content: "<p>content</p>",
+	};
+	const formattedDate = formatDate(post.publishedAt ?? "1900-01-01");
+	const currentUrl = encodeURIComponent(`https://yumilc.info/news/${postId}`);
+	const twitterUserName = encodeURIComponent("yumILC_");
+	const shareText = encodeURIComponent(
+		`${post.title} - ゆーみるしー
+	@${twitterUserName}
+`,
+	);
+	const tweetUrl = `https://twitter.com/intent/tweet?text=${shareText}&url=${currentUrl}`;
+	const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`;
+
 	return (
 		<div>
 			<div className={mainStyle}>
@@ -77,6 +225,47 @@ export default function Home(): JSX.Element {
 							</HoverGrowWrapper>
 						</div>
 					</div>
+				</div>
+				<h1 className={`${Montserrat400.className} ${headingStyle}`}>News</h1>
+				<div>
+					<h1 className={`${ZenMaruGothic400.className} ${titleStyle}`}>
+						{post.title}
+					</h1>
+					<div
+						className={css({
+							display: "flex",
+							justifyContent: "space-between",
+							marginBottom: "20px",
+						})}
+					>
+						<h2 className={`${ZenMaruGothic400.className} ${dateStyle}`}>
+							{formattedDate}
+						</h2>
+						<div className={css({ display: "flex" })}>
+							<div>
+								<Link href={tweetUrl} target="_blank">
+									<div className={snsImageStyle}>
+										<Image src="/sns/x.svg" alt="Xにシェアする" fill />
+									</div>
+								</Link>
+							</div>
+							<div>
+								<Link href={facebookUrl} target="_blank">
+									<div className={snsImageStyle}>
+										<Image
+											src="/sns/facebook.svg"
+											alt="Facebookにシェアする"
+											fill
+										/>
+									</div>
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div
+						className={`${ZenMaruGothic400.className} ${articleStyle}`}
+						dangerouslySetInnerHTML={{ __html: post.content }}
+					/>
 				</div>
 			</div>
 		</div>
